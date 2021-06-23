fda0d40f564ba8c3a78f1b5e7688fb848ff4f6db
Rashid Malik Awan <itsmalikrashid@gmail.com>
2021-06-23 03:19:12 -0700

    New Project Check in
    code add to git

---
+++ EmployeeManagementModels/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagementModels
+{
+    public class Employee
+    {
+        public int Emplyeeid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public int Departmentid { get; set; }
+        public string PhotoPath { get; set; }
+
+    }
+}
---
+++ EmplyeeManagement.Web/Pages/EmplyeeListBase.cs
@@ -0,0 +1,51 @@
+﻿using EmployeeManagementModels;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmplyeeManagement.Web.Pages
+{
+    public class EmplyeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employee { get; set; }
+        //protected override Task OnInitializedAsync()
+        //{
+        //    LoadEmpoyees();
+        //    return base.OnInitializedAsync();
+        //}
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Run(LoadEmpoyees);
+           
+        }
+        private void LoadEmpoyees()
+        {
+            System.Threading.Thread.Sleep(3000);
+            Employee e1 = new Employee
+            {
+                Emplyeeid = 1,
+                FirstName="Rashid",
+                LastName="Malik",
+                Email="itsmalikrashid@gmail.com",
+                DateOfBirth = new DateTime(1992,10,1),
+                Gender=Gender.Male,
+                Departmentid =1,
+                PhotoPath= "images/2021-03-19.jpg"
+            };
+            Employee e2 = new Employee
+            {
+                Emplyeeid = 2,
+                FirstName = "Sara",
+                LastName = "Khan",
+                Email = "SKhan@yopmail.com",
+                DateOfBirth = new DateTime(1995, 11, 10),
+                Gender = Gender.Female,
+                Departmentid = 2,
+                PhotoPath = "images/sara.jpg"
+            };
+            Employee = new List<Employee> { e1 ,e2};
+        }
+    }
+}
